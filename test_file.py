20abcae3ebfe65d94858b581db1cb7aee202ec85
Joel Madrigal <jmadriga@iwu.edu>
2024-03-21 22:07:01 -0500

    Reapply "Reapply "Reapply "Commit of final copy, Edit feature and Bootstrap is in and tests are all in and working. Time to see if Actions works."""
    This reverts commit f73d82c707f6a1076ff8f1d180aa1e9db81a45bd.

--- test_file.py
+++ test_file.py
@@ -27,6 +27,7 @@ class FlaskrTestCase(unittest.TestCase):
             text='<strong>HTML</strong> allowed here',
             category='A category'
         ), follow_redirects=True)
+
         assert b'No entries here so far' not in rv.data
         assert b'&lt;Hello&gt;' in rv.data
         assert b'<strong>HTML</strong> allowed here' in rv.data
@@ -34,20 +35,22 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_delete(self):
         self.app.post('/add', data=dict(
-            title='<Hello>',
-            text='<strong>HTML</strong> allowed here',
+            title='thing',
+            text='stuff',
             category='A category'
         ), follow_redirects=True)
 
         rv = self.app.post('/delete', data=dict(
-            title='<Hello>',
-            text='<strong>HTML</strong> allowed here',
+            title='thing',
+            text='stuff',
             category='A category',
             id='1'
         ), follow_redirects=True)
 
         assert b'No entries here so far' in rv.data
-        assert b'&lt;Hello&gt;' not in rv.data
+        assert b'stuff' not in rv.data
+        assert b'A category' not in rv.data
+        assert b'thing' not in rv.data
 
     def test_filter(self):
         self.app.post('/add', data=dict(
@@ -60,7 +63,7 @@ class FlaskrTestCase(unittest.TestCase):
             category='A category'
         ), follow_redirects=True)
 
-        assert b'Unbelievable.  No entries here so far' not in rb.data
+        assert b'&lt;Hello&gt;' in rb.data
         assert b'<strong>HTML</strong> allowed here' in rb.data
         assert b'A category' in rb.data
 
@@ -68,9 +71,72 @@ class FlaskrTestCase(unittest.TestCase):
             category='wrong category'
         ), follow_redirects=True)
 
-        assert b'Unbelievable.  No entries here so far' in rz.data
+        assert b'&lt;Hello&gt;' not in rz.data
         assert b'<strong>HTML</strong> allowed here' not in rz.data
         assert b'A category' not in rz.data
+
+    def test_edit_page(self):
+        self.app.post('/add', data=dict(
+            title='<Hello>',
+            text='<strong>HTML</strong> allowed here',
+            category='A category'
+        ), follow_redirects=True)
+
+        rv = self.app.post('/edit', data=dict(
+            id='1'
+        ), follow_redirects=True)
+
+        assert b'&lt;Hello&gt;' in rv.data
+        assert b'&lt;strong&gt;HTML&lt;/strong&gt; allowed here' in rv.data
+        assert b'A category' in rv.data
+
+        rb = self.app.post('/edit', data=dict(
+            id='2'
+        ), follow_redirects=True)
+
+        assert b'&lt;Hello&gt;' not in rb.data
+        assert b'&lt;strong&gt;HTML&lt;/strong&gt; allowed here' not in rb.data
+        assert b'A category' not in rb.data
+
+    def test_edit(self):
+        self.app.post('/add', data=dict(
+            title='<Hello>',
+            text='<strong>HTML</strong> allowed here',
+            category='A category'
+        ), follow_redirects=True)
+
+        rv = self.app.post('/', data=dict(
+            title='<Helloowi>',
+            text='<strong>HTMLio</strong> allowed here',
+            category='A categories',
+            id='1'
+        ), follow_redirects=True)
+
+        assert b'&lt;Hello&gt;' not in rv.data
+        assert b'&lt;strong&gt;HTML&lt;/strong&gt; allowed here' not in rv.data
+        assert b'A category' not in rv.data
+
+        assert b'&lt;Helloowi&gt;' in rv.data
+        assert b'<strong>HTMLio</strong> allowed here' in rv.data
+        assert b'A categories' in rv.data
+
+    def test_return(self):
+        self.app.post('/add', data=dict(
+            title='<Hello>',
+            text='<strong>HTML</strong> allowed here',
+            category='A category'
+        ), follow_redirects=True)
+
+        self.app.post('/filter', data=dict(
+            category='A category'
+        ), follow_redirects=True)
+
+        rv = self.app.post('/return', follow_redirects=True)
+
+        assert b'No entries here so far' not in rv.data
+        assert b'&lt;Hello&gt;' in rv.data
+        assert b'<strong>HTML</strong> allowed here' in rv.data
+        assert b'A category' in rv.data
 
 
 if __name__ == '__main__':
